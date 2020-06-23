2eba6e6bfe6492557e2fc2b45d08e1d73f759c93
Vasu Bansal <vasubansal1998@gmail.com>
2020-06-23 16:08:57 +0530

    Updated 23/06

--- Algorithms/Binary Search/012 peakElement.cpp
+++ Algorithms/Binary Search/012 peakElement.cpp
@@ -2,44 +2,43 @@
 using namespace std;
 #define loop(i, n) for(int i=0; i<n; i++)
 
-int ans(vector<int>& v){
+
+int ans(vector<int>& v) {
 
     int n = v.size();
-    int start(0), last(n-1);
-    if(n==1)
-        return 0;
-    if(n==2)
-        return v[0]>v[1]?0:1;
-    while(start<=last){
-        int mid = start+(last-start)/2;
+    int lo = 0;
+    int hi = n-1;
 
-        if(mid!=0 && mid!=n-1){
-            if(v[mid]>=v[mid-1] && v[mid]>=v[mid+1]){
+    while(lo<hi){
+
+        int mid = lo+(hi-lo)/2;
+
+        if(mid==0){
+            if(v[mid]>v[mid+1])
                 return mid;
-            }else if(v[mid+1]>v[mid]){
-                start = mid+1;
-            }else if(v[mid-1]>v[mid]){
-                last = mid-1;
-            }
-        }else{
-            if(mid==0){
-                if(v[mid]>v[mid+1])
-                    return mid;
-                else
-                    start = mid+1;
-            }else if(mid==n-1){
-                if(v[mid]>v[mid-1])
-                    return mid;
-                else
-                    last = mid-1;
-            }
+            else
+                lo = mid+1;
+        }else if(mid==n-1){
+            if(v[mid]>v[mid-1])
+                return mid;
+            else
+                hi = mid-1;
+        }
 
+        else if(v[mid]>v[mid-1] && v[mid]>v[mid+1]){
+            return mid;
+        }else if(v[mid]<v[mid-1]){
+            hi = mid-1;
+        }else if(v[mid]<v[mid+1]){
+            lo = mid+1;
         }
 
     }
 
+    return lo;
 
 }
+
 int main(){
 
     int t; cin>>t;
