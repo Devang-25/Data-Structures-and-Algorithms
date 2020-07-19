8b45e5532c1753836928faf41c63c36edb8d42e0
Vasu Bansal <vasubansal1998@gmail.com>
2020-07-19 18:57:59 +0530

    19/7

--- Datastructures/005 Trees/015 validateBST.cpp
+++ Datastructures/005 Trees/015 validateBST.cpp
@@ -11,6 +11,85 @@
  */
 class Solution {
 public:
+    TreeNode* getPred(TreeNode* root){
+
+        if(root==nullptr || root->left==nullptr) return nullptr;
+
+        auto temp = root->left;
+        while(temp->right && temp->right!=root)
+            temp = temp->right;
+
+        return temp;
+
+    }
+    bool isValidBST(TreeNode* root) {
+
+        // runtime error, if we break when we get false. Because we cannot change the tree
+
+        TreeNode* curr = root;
+        TreeNode* prev = nullptr;
+
+        bool res = true;
+
+        while(curr){
+
+            if(curr->left==nullptr){
+
+                // visit curr
+                if(prev && prev->val>=curr->val){
+                    res =  false;
+                }
+
+
+                prev = curr;
+                curr = curr->right;
+
+            }else{
+
+                TreeNode* pred = getPred(curr);
+
+                if(pred->right==nullptr){
+
+                    pred->right = curr;
+                    curr = curr->left;
+
+                }else{
+
+                    pred->right = nullptr;
+                    // visit curr
+                    if(prev && prev->val>=curr->val){
+                        res = false;
+                    }
+
+                    prev = curr;
+                    curr = curr->right;
+
+                }
+
+            }
+
+        }
+
+        return res;
+
+    }
+};
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
     bool isValidBST(TreeNode* root) {
 
         TreeNode* prev = nullptr;
